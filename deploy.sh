cec53f8c7fb07133b749e7d60b05b7a4b64d136b
khalidfathyFL <khalidfathy077@gmail.com>
2024-12-09 23:35:54 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e # Exit immediately if a command fails
+
 echo "=============================================================="
 echo " Checking Node.js & NPM version..."
 echo "=============================================================="
@@ -9,6 +11,7 @@ echo "=============================================================="
 echo " Installing Hexo & NPM modules..."
 echo "=============================================================="
 npm install --silent
+npm install hexo-cli -g
 
 generate_theme() {
     THEME=$1
@@ -18,11 +21,17 @@ generate_theme() {
     echo "=============================================================="
     echo " Generating content for $THEME..."
     echo "=============================================================="
-    hexo config url $URL
-    hexo config theme_config.scheme $THEME
-    hexo config theme_config.sidebar.position $SIDEBAR
-    hexo clean && hexo g
-    mv -v public $THEME || { echo "Error moving public to $THEME"; exit 1; }
+    hexo config url "$URL"
+    hexo config theme_config.scheme "$THEME"
+    hexo config theme_config.sidebar.position "$SIDEBAR"
+    hexo clean && hexo generate
+
+    if [ -d public ]; then
+        mv -v public "$THEME"
+    else
+        echo "Error: 'public' directory does not exist for $THEME"
+        exit 1
+    fi
 }
 
 # Generate themes
@@ -30,20 +39,22 @@ generate_theme Muse https://theme-next.js.org/muse right
 generate_theme Mist https://theme-next.js.org/mist right
 generate_theme Pisces https://theme-next.js.org/pisces left
 
-# Prepare Gemini
 echo "=============================================================="
 echo " Preparing content for Gemini..."
 echo "=============================================================="
 hexo config url https://theme-next.js.org
 hexo config theme_config.scheme Gemini
-hexo clean && hexo g
+hexo clean && hexo generate
 
 echo "=============================================================="
 echo " Moving all themes to public directory..."
 echo "=============================================================="
-mv -v muse mist pisces -t public || { echo "Error moving themes to public"; exit 1; }
+mkdir -p public
+mv -v Muse Mist Pisces public
 
-# Add robots.txt
+echo "=============================================================="
+echo " Adding robots.txt to public directory..."
+echo "=============================================================="
 echo "User-agent: *
 Disallow: /page/*/
 Disallow: /archives/*
@@ -53,5 +64,5 @@ Disallow: /pisces/*
 Host: https://theme-next.js.org" > public/robots.txt
 
 echo "=============================================================="
-echo " Done. Beginning to deploy site..."
+echo " Done. Ready for deployment!"
 echo "=============================================================="
